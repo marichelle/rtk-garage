79782ba22de1cf5359683e9f5bbb12280bd75250
Maya Estalilla <mestalilla@gmail.com>
2023-08-09 20:07:17 -0400

    Connect React + Redux store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 
 import App from './App'
+import { store } from './store'
 import './styles/global.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <>
+  <Provider store={store}>
     <App />
-  </>
+  </Provider>
 )
